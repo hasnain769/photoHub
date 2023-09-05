02a698ddc1af03db31e6262b15cf8b05ccc28b54
syed hasnain <syedhasnain769@gmail.com>
2023-09-05 20:39:22 +0500

    ...

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+
 import UploadImage from '@/components/ui/uploadImage'
+import cloudinary from 'cloudinary'
 export default function page() {
+    cloudinary.v2.search
+  .expression('resource_type:image ')
+  .sort_by('public_id','desc')
+  .max_results(30)
+  .execute()
+  
   return (
     <div className=''>
          <div className='flex justify-between text-4xl   font-semibold px-3'>
